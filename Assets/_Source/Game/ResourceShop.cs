283657dad0457ecff98983b98f7afae6d2586b53
Anastasia Samilyk <asamilyk@yandex.ru>
2024-02-21 12:04:36 +0300

    add resourse shop

---
+++ Assets/_Source/Game/ResourceShop.cs
@@ -0,0 +1,65 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Diagnostics;
+
+namespace _Source.Game
+{
+    public class ResourceShop : MonoBehaviour
+    {
+        [SerializeField] private GameResource _resource;
+        [SerializeField] private ResourceBank _bank;
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private int _currPrice = 5;
+
+        private int Price => _currPrice * _bank.GetResource(_bank.GetLevel(_resource)).Value;
+
+        void Start()
+        {
+            _text.text = $"Add level to\n {_resource.ToString()}\n {Price} coins";
+        }
+
+        /// <summary>
+        /// Allows to improve the resource level
+        /// </summary>
+        public void Improve()
+        {
+            if (_bank.GetResource(_resource).Value >= Price)
+            {
+                FillButtom(Color.magenta);
+                _bank.GetResource(_resource).Value -= Price;
+                _bank.GetResource(_bank.GetLevel(_resource)).Value++;
+            }
+            else
+            {
+                FillButtom(Color.black);
+            }
+        }
+
+        /// <summary>
+        /// Paints the button in the desired color
+        /// </summary>
+        /// <param name="color"></param>
+        private void FillButtom(Color color)
+        {
+            _button.interactable = false;
+            StartCoroutine(FillButtomCoroutine(color));
+        }
+
+        private IEnumerator FillButtomCoroutine(Color color)
+        {
+            _button.image.color = color;
+            int timeForColor = 50;
+            for (int i = 0; i < timeForColor; ++i)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            _button.image.color = Color.white;
+            _button.interactable = true;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
